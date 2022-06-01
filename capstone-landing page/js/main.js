408ac8f85ce871e43a8a4dcebc997968b60fe9e4
NurulShafirah13 <79590008+NurulShafirah13@users.noreply.github.com>
2022-06-01 22:03:49 +0700

    Add files via upload

---
+++ capstone-landing page/js/main.js
@@ -0,0 +1,104 @@
+
+$(document).ready(function() {
+	/*-------------------navbar shrink-----------------*/
+	$(window).on("scroll", function(){
+		if($(this).scrollTop() > 90){
+			$(".navbar").addClass("navbar-shrink")
+		
+		}
+		else{
+			$(".navbar").removeClass("navbar-shrink")
+			
+	    }
+	});
+
+
+
+	/*------------------- Video Popup-----------------*/
+	const videoSrc = $("#player-1").attr("src"); 
+	$(".video-play-btn .video-popup").on("click", function(){
+		if($(".video-popup").hasClass("open")){
+			$(".video-popup").removeClass("open")
+			$("#player-1").attr("src", "")
+		}
+		else{
+			$(".video-popup").addClass("open")
+			if($("#player-1").attr("src")==''){
+				$("#player-1").attr("src", videoSrc)
+			}
+	    }
+	});
+
+	/*------------------- screenshot Carousel-----------------*/
+
+$('.screenshots-carousel').owlCarousel({
+	loop:true,
+	margin:0,
+	// autoplay:true,
+	responsiveClass:true,
+	responsive:{
+		0:{
+			items:1,
+		},
+		600:{
+			items:2,
+
+		},
+		1000:{
+			items:3,
+		}
+	}
+});
+
+	/*------------------- team Carousel-----------------*/
+
+$('.team-carousel').owlCarousel({
+	loop:true,
+	margin:0,
+	// autoplay:true,
+	responsiveClass:true,
+	responsive:{
+		0:{
+			items:1,
+		},
+		600:{
+			items:2,
+
+		},
+		1000:{
+			items:3,
+		}
+	}
+});
+
+	/*------------------- team hope Carousel-----------------*/
+
+$('.vision-carousel').owlCarousel({
+	loop:true,
+	margin:0,
+	// autoplay:true,
+	responsiveClass:true,
+	responsive:{
+		0:{
+			items:1,
+		},
+		600:{
+			items:2,
+
+		},
+		1000:{
+			items:3,
+		}
+	}
+});
+	/*------------------- page scrolling-----------------*/
+	$.scrollIt({
+		topOffset: -50
+	})
+
+	/*------------------- screenshot Carousel-----------------*/
+	$(".nav-link").on("click", function(){
+		$(".navbar-collapse").collapse("hide");
+	})
+
+});
